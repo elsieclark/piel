82e59fad59d4783e0acb8f39fde53ce126ac54ec
willdavidc <willdavidc@gmail.com>
2016-11-06 15:13:16 -0500

    Multithread commandsocket

--- scripts/commandsocket.py
+++ scripts/commandsocket.py
@@ -1,13 +1,45 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/env python
 import time
-
+import rospy
+import threading
+from std_msgs.msg import String
 from socketIO_client import SocketIO, LoggingNamespace
+
+
 socketIO = SocketIO('localhost:3000')
 
-def onCommand(*args):
-    print(args[0].split('+')[0])
+commands = "0+0"
 
-while (True):
-    socketIO.on("commands", onCommand)
-    socketIO.wait(seconds=1)
-    socketIO.off("sequencePi")
+def onCommand(*args):
+    global commands
+    commands = args[0]
+    
+class SocketThread(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+    def run(self):
+            socketIO.on("commands", onCommand)           
+            socketIO.wait()
+
+socketThread = SocketThread("socketThread")
+socketThread.start()
+
+def talker():
+    global commands
+    pub = rospy.Publisher('commands', String, queue_size=10)
+    rospy.init_node('commandsocket', anonymous=True)
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        rospy.loginfo(commands)
+        pub.publish(commands)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+    
+    
+socketThread.join()
