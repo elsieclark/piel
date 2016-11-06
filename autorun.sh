13dbd4886253ec85cf5e89002dec18ec5ddc2ab9
willdavidc <willdavidc@gmail.com>
2016-11-06 15:57:59 -0500

    Update autorun procedure
    Update autorun procedure to initiate commandsocket.

--- autorun.sh
+++ autorun.sh
@@ -2,10 +2,14 @@
 
 cd /home/pi/Desktop/piel/scripts
 
-tmux new -s server -d
-tmux new -s motors -d
+tmux new -s node_server -d
 
-tmux send -t motors.0 'python3 motors.py' ENTER
-tmux send -t server.0 'node server.js' ENTER
+tmux new -s ros_core -d
+tmux new -s ros_py_motors -d
+tmux new -s ros_py_commandsocket -d
 
+tmux send -t node_server.0 'node server.js' ENTER
 
+tmux send -t ros_core.0 'roscore' ENTER
+tmux send -t ros_py_motors.0 'python3 motors.py' ENTER
+tmux send -t ros_py_commandsocket.0 'rosrun piel scripts/commandsocket.py' ENTER
