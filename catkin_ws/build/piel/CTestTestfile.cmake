932af02cc2edc7d1b9132a45927f6ec09d471efe
willdavidc <willdavidc@gmail.com>
2016-11-06 21:23:54 +0000

    Add catkin workspace

---
+++ catkin_ws/build/piel/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Desktop/piel/catkin_ws/src/piel
+# Build directory: /home/pi/Desktop/piel/catkin_ws/build/piel
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
